2e4fb9d7b6b11dae500570d688fd514b382bd9f6
Livia <livia.fares97@gmail.com>
2021-03-07 20:47:07 -0300

    Add shuffle and reset functions

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 const cards = document.querySelectorAll('.card');
 let hasFlippedCard = false;
 let firstCard, secondCard;
-
+let blockBoard = false;
 
 function flipCard() {
+    if (blockBoard) {
+        return;
+    }
     this.classList.add('flip');
     if (!hasFlippedCard) {
         hasFlippedCard = true;
@@ -26,14 +29,31 @@ function checkForMatch() {
 function disableCards() {
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
 }
 
 function unflipCards() {
+    blockBoard = true;
+
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
+        resetBoard();
     }, 1000)
 }
+
+function resetBoard() {
+    [hasFlippedCard, blockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+(function shuffleBoard() {
+    cards.forEach((card) => {
+        let randomPosition = Math.floor(Math.random() * 12);
+        card.style.order = randomPosition;
+    })
+})();
 
 cards.forEach((card) => {
     card.addEventListener('click', flipCard);
